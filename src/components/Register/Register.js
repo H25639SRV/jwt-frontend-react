4593859cf78dde7b5090d5949431ba774897dd50
H25639SRV <giahuytruong2506@gmail.com>
2024-10-27 00:17:24 +0700

    register

---
+++ src/components/Register/Register.js
@@ -0,0 +1,76 @@
+import "./Register.scss";
+import { useHistory } from "react-router-dom";
+
+const Register = (props) => {
+  let history = useHistory();
+
+  const handleLogin = () => {
+    history.push("/login");
+  };
+  return (
+    <div className="register-container">
+      <div className="container">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+            <div className="brand">H25639SRV</div>
+            <div className="detail">khong muon hoc it dau huhu</div>
+          </div>
+          <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+            <div className="brand d-sm-5 d-sm-none">H25639SRV</div>
+            <div className="form-group">
+              <label>Email:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Email address"
+              />
+            </div>
+            <div className="form-group">
+              <label>Phone number:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Phone number"
+              />
+            </div>
+            <div className="form-group">
+              <label>Username:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+              />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <div className="form-group">
+              <label>Re-enter your password:</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Re-enter your password"
+              />
+            </div>
+
+            <button className="btn btn-primary">Register</button>
+
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success" onClick={() => handleLogin()}>
+                Already have an account?
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
