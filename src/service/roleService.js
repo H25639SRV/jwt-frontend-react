95474cc14effcfdbd99e6f03c59dfd3e1490c28a
H25639SRV <giahuytruong2506@gmail.com>
2024-11-18 23:57:03 +0700

    read&delete roles

--- src/service/roleService.js
+++ src/service/roleService.js
@@ -3,5 +3,14 @@ import axios from "../setup/axios";
 const createRoles = (roles) => {
   return axios.post("/api/v1/roles/create", [...roles]);
 };
+const fetchAllRole = () => {
+  return axios.get(`/api/v1/roles/read`);
+};
 
-export { createRoles };
+const deleteRole = (role) => {
+  return axios.delete("/api/v1/roles/delete", {
+    data: { id: role.id },
+  });
+};
+
+export { createRoles, fetchAllRole, deleteRole };
